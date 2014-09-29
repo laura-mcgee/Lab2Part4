915060b125230bfe03d20fb830873c421482433c
Laura McGee <laura.mcgee12@gmail.com>
2014-09-28 19:17:39 -0700

    laura conflict

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -48,7 +48,7 @@ public void onModuleLoad() {
     LoginServiceAsync loginService = GWT.create(LoginService.class);
     loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
       public void onFailure(Throwable error) {
-    	  System.out.println("conflict test-ian");
+    	  System.out.println("conflict test-laura");
     	  handleError(error);
       }
 
